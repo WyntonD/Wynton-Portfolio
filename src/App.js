ceceee900af83b1f25f1dd7d5b34392ce8cdc03f
WD$ <wyntonmdavis@gmail.com>
2021-01-22 13:01:54 -0500

    inital page setups

--- src/App.js
+++ src/App.js
@@ -3,24 +3,30 @@ import { BrowserRouter as Router,
         Switch,
         Route
         } from 'react-router-dom';
-
 import Nav from './components/Nav/nav';
-import TopHeader from './components/TopHeader/topheader';     
+import TopHeader from './components/TopHeader/topheader';
+import Home from './pages/Home/home';
+import About from './pages/About/about';
+import Resume from './pages/Resume/resume';
+import Skills from './pages/Skillset/skills';
+import Projects from './pages/Projects/projects';
+import Contact from './pages/Contact/contact';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <TopHeader />
-        <Nav/>
+        {/* <TopHeader />
+        <Nav/> */}
         <Switch>
-          <Route exact path="/home" component={} />
-          <Route exact path="/about" component={} />
-          <Route exact path="/resume" component={} />
-          <Route exact path="/skills" component={} />
-          <Route exact path="/projects" component={} />
-          <Route exact path="/contact" component={} />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/resume" component={Resume} />
+          <Route exact path="/skills" component={Skills} />
+          <Route exact path="/projects" component={Projects} />
+          <Route exact path="/contact" component={Contact} />
         </Switch>
       </Router>
     </div>
