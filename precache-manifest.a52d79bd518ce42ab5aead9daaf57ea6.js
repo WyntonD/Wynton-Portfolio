608d524b2861350619ccb38bd589c0c7ab0ba5f7
WD$ <wyntonmdavis@gmail.com>
2021-03-21 16:39:24 -0400

    Updates

---
+++ precache-manifest.a52d79bd518ce42ab5aead9daaf57ea6.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6547f29669c349b2aed586359a5f2207",
+    "url": "/Wynton-Portfolio.git/index.html"
+  },
+  {
+    "revision": "180253cc930de4556f70",
+    "url": "/Wynton-Portfolio.git/static/css/main.d2ef2f63.chunk.css"
+  },
+  {
+    "revision": "dd49d52905112f0db2fa",
+    "url": "/Wynton-Portfolio.git/static/js/2.fd8db622.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/Wynton-Portfolio.git/static/js/2.fd8db622.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "180253cc930de4556f70",
+    "url": "/Wynton-Portfolio.git/static/js/main.cbb7cb63.chunk.js"
+  },
+  {
+    "revision": "c2fa51a3b6e71273886c",
+    "url": "/Wynton-Portfolio.git/static/js/runtime-main.de1da879.js"
+  },
+  {
+    "revision": "c1dc09327168f4eaa57ff01f113d96b3",
+    "url": "/Wynton-Portfolio.git/static/media/cta-illustration.c1dc0932.svg"
+  },
+  {
+    "revision": "120272a776de3f9a464aab5c269d0ffb",
+    "url": "/Wynton-Portfolio.git/static/media/feature-tile-icon-01.120272a7.svg"
+  },
+  {
+    "revision": "bd39f30434175e7c8b1d1c74fa6569fd",
+    "url": "/Wynton-Portfolio.git/static/media/feature-tile-icon-02.bd39f304.svg"
+  },
+  {
+    "revision": "66f37ba5c7e4b6648f04883fa637e634",
+    "url": "/Wynton-Portfolio.git/static/media/feature-tile-icon-03.66f37ba5.svg"
+  },
+  {
+    "revision": "836acd100a942420f8ab4e6c5e4162d8",
+    "url": "/Wynton-Portfolio.git/static/media/feature-tile-icon-04.836acd10.svg"
+  },
+  {
+    "revision": "fa9ba00b178abc7bd2e02bd986e91fe3",
+    "url": "/Wynton-Portfolio.git/static/media/feature-tile-icon-05.fa9ba00b.svg"
+  },
+  {
+    "revision": "6a1776963deb39d710bae80bb449aebe",
+    "url": "/Wynton-Portfolio.git/static/media/feature-tile-icon-06.6a177696.svg"
+  },
+  {
+    "revision": "ff7ad4dcfa771acf1044c23de6ae034b",
+    "url": "/Wynton-Portfolio.git/static/media/google.ff7ad4dc.jpg"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/Wynton-Portfolio.git/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/Wynton-Portfolio.git/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "1b3c8b39dffc6bee07b22df571d90941",
+    "url": "/Wynton-Portfolio.git/static/media/liriscreenshot.1b3c8b39.jpg"
+  },
+  {
+    "revision": "2810a88b1d6365b363caeabe678f1629",
+    "url": "/Wynton-Portfolio.git/static/media/logo.2810a88b.svg"
+  },
+  {
+    "revision": "35d629130d2ac898091faea7484b6382",
+    "url": "/Wynton-Portfolio.git/static/media/wyntlugerscreenshot.35d62913.jpg"
+  }
+]);
